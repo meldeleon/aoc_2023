e826a95afc85f4b092645cbac0749cd917f8961a
leomeli <melissadeleon@gmail.com>
2023-12-28 14:03:14 -0800

    solving day 10 pt 1

--- butts.js
+++ butts.js
@@ -1,52 +1,16 @@
-let emptyCols = [3, 7]
-let galaxies = [
-  [0, 3],
-  [1, 7],
-  [2, 0],
-  [4, 6],
-  [5, 1],
-  [6, 9],
-  [8, 7],
-  [9, 0],
-  [9, 4],
-]
+let visited = new Set()
 
-function findPositionInEmptyArr(galaxyPos, emptyArr) {
-  let count = 0
-  let start = 0
-  let end = emptyArr.length - 1
-  while (true) {
-    //find the halfway pt
-    let midPoint = Math.floor((start + end) / 2)
-    //we are checking if the pt is lower than our number, but the next point must be higher
-    let lowerCheck = emptyArr[midPoint]
-    let upperCheck = emptyArr[midPoint + 1]
-    console.log({ start }, { end }, { midPoint })
-    console.log({ lowerCheck }, { upperCheck })
-    //edge case: the position is less than index 0
-    if (galaxyPos < emptyArr[0]) {
-      return 0
+visited.add('1,1')
+visited.add('1,2')
+
+let test = new Set('1,2', '1,3')
+
+function moveForward(pipes){
+  for (pipe of pipes){
+    if (!visited.has(pipe)){
+      return pipe
     }
-    //edge case: poisiton is greater than last val in array
-    else if (galaxyPos >= emptyArr[emptyArr.length - 1]) {
-      console.log(`butts2`)
-      if (midPoint === 0) {
-        return 1
-      } else {
-        return emptyArr.length - 1
-      }
-    } else if (galaxyPos > lowerCheck && galaxyPos < upperCheck) return midPoint
-    else if (galaxyPos < lowerCheck) end = midPoint - 1
-    else if (galaxyPos > upperCheck) start = midPoint + 1
-    else {
-      console.log(
-        `something weird happened with input ${galaxyPos}, ${emptyArr}`
-      )
-    }
-    count++
-    if (count === 10) break
   }
 }
 
-let emptyRows = [2, 5, 8, 12]
-console.log(findPositionInEmptyArr(3, emptyRows))
+console.log(moveForward(['1,2','1,4']))
--- day_10_pt1.js
+++ day_10_pt1.js
@@ -1,132 +1,166 @@
-//constants for mapping data
-const pipeTypes = {
-  "|": ["N", "S"],
-  "-": ["E", "W"],
-  L: ["N", "E"],
-  J: ["N", "W"],
-  7: ["S", "W"],
-  F: ["S", "E"],
-  ".": null,
-}
-
-//establish order for directions
-const directions = ["N", "E", "S", "W"]
-
-//define direction matches i.e. if a pipe flow south and the pipe below it flows north
-const matches = {
-  N: "S",
-  S: "N",
-  E: "W",
-  W: "E",
-}
-
-let startingPipe
-
-//create pipemap data structure
-const pipeMap = require("fs")
-  .readFileSync("day_10_input.txt")
-  .toString()
-  .split(/\r\n/)
-  .map((row, indexRow) => {
-    let segmentedRow = row.split("")
-    segmentedRow.forEach((x, indexCol) => {
-      if (x === "S") {
-        startingPipe = [parseInt(indexRow), parseInt(indexCol)]
+class Graph {
+  constructor() {
+      this.pipes = new Map()
+  }
+  getPipe(p) {
+      return this.pipes.get(p)
+  }
+  addPipe(p) {
+      this.pipes.set(p, new Set())
+  }
+  addConnector(p1, p2) {
+      try
+      {
+      this.pipes.get(p1).add(p2)
+      } 
+      catch(error){
+          console.log(`error connecting ${p1}, to ${p2}, ${error}`)
       }
-    })
-    return segmentedRow
-  })
+  }
 
-//create a copy of the map for marking the "inner loop"
-let loopMap = returnEmptyMap(pipeMap.length, pipeMap[0].length)
-//mark the starting value of loopMap
-markLoopMap(startingPipe)
-let loopStack = findConnectingPipes(startingPipe)
-console.log({ loopStack })
-let visitedTiles = new Set()
+}
 
-while (true) {
-  //grab a connected tile from the stack
-  let connectedTile = loopStack.shift()
-  //if we have visited a tile, add to set
-  visitedTiles.add(connectedTile.join(","))
-  console.log({ connectedTile })
-  //mark tile as connected
-  markLoopMap(connectedTile)
-  //find where that tile has connections and push to stack
-  let newConnections = findConnectingPipes(connectedTile)
-  newConnections.forEach((connectedTile) => {
-    if (!visitedTiles.has(connectedTile.join(","))) {
-      loopStack.push(connectedTile)
+//we will find S in input parsing
+let startingPipeLocation
+
+//parse input => 2D matrix of all the pipes
+const pipesMatrix = require("fs").readFileSync("day_10_input.txt")
+.toString()
+.split(/\n/)
+.map((row, indexRow) => {
+  let segmentedRow = row.split("")
+  segmentedRow.forEach((x, indexCol) => {
+    if (x === "S") {
+      startingPipeLocation = [parseInt(indexRow), parseInt(indexCol)]
     }
   })
-  console.table(loopMap)
-  console.log({ loopStack })
-  if (compareArr(connectedTile, startingPipe) || loopStack.length <= 0) {
-    break
+  return segmentedRow
+}
+)
+
+//console.table(pipesMatrix)
+
+const pipeGraph = new Graph()
+//store our pipes in the graph
+for (let row = 0; row < pipesMatrix.length; row++) {
+  for (let col = 0; col < pipesMatrix[row].length; col++){
+      pipeGraph.addPipe([row, col].join(","))
+  }
+}
+//add connections
+[...pipeGraph.pipes.keys()].forEach(pipeLocation => {
+  let [row, col] = pipeLocation.split(",")
+  row = parseInt(row)
+  col = parseInt(col)
+  let connectedPipes
+  if (row === startingPipeLocation[0] && col === startingPipeLocation[1]){
+    connectedPipes = findStartingPipeConnections(row, col)
+  } else{
+    connectedPipes = findConnectedPipes(row, col)
+  }
+  connectedPipes.forEach(connectedPipe => {
+      pipeGraph.addConnector(pipeLocation, connectedPipe.join(','))
+  })
+})
+
+
+//start with starting location, and then map store distance from Start in an array
+
+//we will go left and right, incrementing the distance value and when the two paths merge we will stop 
+let startStr = startingPipeLocation.join(",")
+let [left, right] = pipeGraph.getPipe(startStr)
+let lastLeft = startStr
+let lastRight = startStr
+let steps = 1
+
+while (true || count === 100000){
+  if (right === left && right !== undefined) {
+      //console.log(`${right} === ${left}`)
+      break
+  }
+
+  let leftConnected = pipeGraph.getPipe(left)
+  nextLeft = moveForward(leftConnected, lastLeft)
+  lastLeft = left
+  left = nextLeft
+  let rightConnected = pipeGraph.getPipe(right)
+  nextRight = moveForward(rightConnected, lastRight)
+  lastRight = right
+  right = nextRight
+  steps ++
+}
+
+console.log(`the solution is ${steps}`)
+
+//helper functions for traversing graph
+function moveForward(pipes, lastPipe){
+  for (pipe of pipes){
+     if (lastPipe !== pipe){
+      return pipe
+    }
   }
 }
 
-function findConnectingPipes(originTile) {
-  let connected = []
-  let originCol = originTile[0]
-  let originRow = originTile[1]
-  let originTileType = pipeMap[originCol][originRow]
-  let originFlowDirections = pipeTypes[originTileType]
-  console.log()
-  let tileLocations = [
-    [originCol + 1, originRow],
-    [originCol, originRow + 1],
-    [originCol - 1, originRow],
-    [originCol, originRow - 1],
+
+//helper functions for building graph
+function findConnectedPipes(row, col){
+  let connectorType = pipesMatrix[row][col]
+  let connectedPipes = []
+  let [up, down, right, left] = [
+      [parseInt(row) - 1, parseInt(col)],
+      [parseInt(row) + 1, parseInt(col)],
+      [parseInt(row), parseInt(col) + 1],
+      [parseInt(row), parseInt(col) - 1],
   ]
-  //iterate over each location, if the location is connected, return connected locations
-  tileLocations.forEach((location, index) => {
-    if (location[0] < pipeMap.length && location[1] < pipeMap[0].length) {
-      let directionFromOriginTile = directions[index]
-      let tileType = pipeMap[location[0]][location[1]]
-      let flowDirections = returnTileTypeDirections(tileType)
-      //we are currently in a connected tile, with flow directions, if a flow direction matches the direction we came from, it is connected
-      let match = matches[directionFromOriginTile]
-      console.log({ tileType }, { flowDirections }, { match })
-      if (flowDirections && flowDirections.includes(match)) {
-        console.log(`a match was found between ${location} and ${originTile}`)
-        connected.push(location)
-      }
+  switch (connectorType) {
+      case "|":
+        connectedPipes.push(up, down)
+        break
+      case "-":
+        connectedPipes.push(right, left)
+        break
+      case "L":
+        connectedPipes.push(up, right)
+        break
+      case "J":
+        connectedPipes.push(left, up)
+        break
+      case "7":
+        connectedPipes.push(down, left)
+        break
+      case "F":
+        connectedPipes.push(down, right)
+      default:
+    }
+    let scrubbedConnectedPipes = connectedPipes.filter((pipe) => checkForEdgeCase(pipe[0], pipe[1]))      
+  return scrubbedConnectedPipes
+}
+function checkForEdgeCase(row, col) {
+return (row >= 0 && row < pipesMatrix.length && col >= 0 && col < pipesMatrix[0].length)
+}
+
+function findStartingPipeConnections(row, col) {
+  //look at all adjacent pipes to the starting pipe
+  let adjacentPipes = [
+    [parseInt(row) - 1, parseInt(col)],
+    [parseInt(row) + 1, parseInt(col)],
+    [parseInt(row), parseInt(col) + 1],
+    [parseInt(row), parseInt(col) - 1],
+  ]
+  // initialized which pipes may be connected
+  let connectedPipes = []
+  //iterate over all adjacent pipes to see what they're connected to
+  adjacentPipes.forEach((adjPipe) => {
+    //check for edge cases, don't check pipes that are off the grid
+    if (checkForEdgeCase(adjPipe[0], adjPipe[1])) {
+      //find what pipes the adjacent pipe is connected to
+      let pipesConnectedToAdjPipe = findConnectedPipes(adjPipe[0], adjPipe[1])
+      //if the adjacent pipe is connected to the starting pipe, push to connected pipes
+      pipesConnectedToAdjPipe.forEach( pipeConnectedToAdjPipe => {
+        if (pipeConnectedToAdjPipe[0] === row && pipeConnectedToAdjPipe[1] === col)connectedPipes.push(adjPipe)
+        
+      })
     }
   })
-  console.log({ connected })
-  return connected
-}
-
-function returnTileTypeDirections(tileType) {
-  for (type in pipeTypes) {
-    let typeStr = `${type}`
-    if (typeStr === tileType) {
-      return pipeTypes[type]
-    }
-  }
-}
-
-function returnEmptyMap(numRow, numCol) {
-  let map = []
-  for (let i = 0; i < numRow; i++) {
-    map.push([])
-    for (let j = 0; j < numCol; j++) {
-      map[i].push(".")
-    }
-  }
-  return map
-}
-
-function markLoopMap(location) {
-  //console.log({ location })
-  //make a copy of the entire row from pipemap
-  let copyOfLocationFromPipeMap = [...pipeMap[location[0]]]
-  //mark in the loopmap a copy of the pipemap value
-  loopMap[location[0]][location[1]] = copyOfLocationFromPipeMap[location[1]]
-}
-
-function compareArr(arrA, arrB) {
-  return arrA.join(",") === arrB.join(",")
+  return connectedPipes
 }
